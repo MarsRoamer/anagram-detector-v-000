6d868f94adea1dd33da0fffd670bac3238ce9f18
Jonathan Tiede <skysoldiermars@gmail.com>
2018-03-07 16:49:18 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,15 +5,26 @@ class Anagram
     @word = word
   end
 
+  # def match(str)
+  #   arr = []
+  #   str.each do |word|
+  #     if word.match(/\b[@word]{@word.length}\b/)
+  #       arr << word
+  #     end
+  #   end
+  #   arr
+  # end
+
+
   def match(str)
     arr = []
     str.each do |word|
-      if word.include?(/\b[@word]{@word.length}\b/)
+
+      if word.sort == @word.sort
         arr << word
       end
     end
     arr
   end
-
 
 end
