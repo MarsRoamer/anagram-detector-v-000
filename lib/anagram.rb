61d0e83c0619a0479e4848a0c17e385f3f82469c
Jonathan Tiede <skysoldiermars@gmail.com>
2018-03-07 16:44:02 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ class Anagram
   def match(str)
     arr = []
     str.each do |word|
-      if word.match(/\b[@word]{@word.length}\b/)
+      if word.include?(/\b[@word]{@word.length}\b/)
         arr << word
       end
     end
